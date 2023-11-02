6fe5f9792c544ec2bdf813f57798f654132cadae
henrya89 <henryarteaga904@gmail.com>
2023-11-01 22:03:59 -0500

    Repositotio

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const {engine} = require('express-handlebars');
+const myconnection = require('express-mysql-connection');
+const bodyParser = require('body-parser');
+const mysql      = require('mysql');
+
+
+
+console.log("crud");
